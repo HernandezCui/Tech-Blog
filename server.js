c680ecf35aa4462fabdf7d71ffd08f44b4a0f41b
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-02-12 21:54:24 -0800

    adding Sequelize session

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
