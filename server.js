2b9243917e42d5ed0fc14d0f4f54795fe319dd59
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-02-12 21:57:59 -0800

    finished server.js

--- server.js
+++ server.js
@@ -22,3 +22,19 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers/"));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("Now listening on port " + PORT));
+});
